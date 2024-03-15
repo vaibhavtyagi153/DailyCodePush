12174c67e86705551f470eba6feb54ecc895d2df
vaibhavtyagi153 <vedvaibhav30@gmail.com>
2024-03-15 14:38:31 +0530

    Next Problem Statement

---
+++ src/com/march/Day3_11032024.java
@@ -0,0 +1,74 @@
+package com.march;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+//palindrome problem statements:
+
+
+
+
+class PalindromeChecks {
+	
+	public void palindromStringCheck(String s) {
+		
+		String revS = reverse(s);
+		if(revS.equals(s)) {
+			System.out.println("Match");
+		} else {
+			System.out.println("noMatch");
+		}
+	}
+	
+	
+	public String reverse(String s) {
+		String reverse = "";
+		
+		for(int i = s.length() - 1; i>=0; i--) {
+			reverse = reverse + s.charAt(i);
+		}
+		
+		return reverse;
+	}
+	
+}
+
+
+public class Day3_11032024 {
+
+	public static void main(String[] args) {
+		
+		String inputString = "";
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Enter a String: ");
+		
+		try {
+			inputString = reader.readLine();
+			
+			System.out.println("You entered: " + inputString);
+			
+		} catch(IOException e) {
+			System.out.println("Error Occured during reading input: " + e.getMessage());
+		} finally {
+			try {
+				reader.close();
+			} catch(IOException e) {
+				System.out.println("Error encounterd while closing reader: " + e.getMessage());
+			}
+		}
+		
+		PalindromeChecks p = new PalindromeChecks();
+		
+		p.palindromStringCheck(inputString);
+		
+		System.out.println("Ending the code");
+		
+		
+		
+	}
+
+}
