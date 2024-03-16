004e3bda2ea074f90f5125ec6371407ea06cbe5f
vaibhavtyagi153 <vedvaibhav30@gmail.com>
2024-03-16 12:53:06 +0530

    temporary workaround for largest palindrome

--- src/com/march/Day5_15032024.java
+++ src/com/march/Day5_15032024.java
@@ -31,12 +31,15 @@ public class Day5_15032024 {
 		
 		PalindromeEU e = new PalindromeEU();
 		
+		int biggestPalindrome = 580085;
+		
 		outerLoop:
 		for (int i = 999; i >= 100; i--) {
 			for (int j = 999; j >= 100; j--) {
 				//System.out.println(i + " " + j);
 				int testPass = i * j;
-				if (e.revAndIntCheck(testPass)) {
+				if (e.revAndIntCheck(testPass) && testPass > biggestPalindrome) {
+					
 					System.out.println("this is ok");
 					System.out.println(i + " " + j);
 					System.out.println("this is fine");
@@ -51,23 +54,23 @@ public class Day5_15032024 {
 		
 		//following not running properly
 		//100 to 999
-		outerLoop:
-		for (int i = 999; i >= 100; i--) {
-			
-			for (int j = 999; j >= 100; j--) {
-				
-				int test_num = i * j;
-				if (p_obj.intPalindromeCheck(test_num)) {
-					
-					//580085
-					//995 583
-					System.out.println(test_num);
-					System.out.println(i + " " + j);
-					break outerLoop;
-				}
-				
-			}
-		}
+//		outerLoop:
+//		for (int i = 999; i >= 100; i--) {
+//			
+//			for (int j = 999; j >= 100; j--) {
+//				
+//				int test_num = i * j;
+//				if (p_obj.intPalindromeCheck(test_num)) {
+//					
+//					//580085
+//					//995 583
+//					System.out.println(test_num);
+//					System.out.println(i + " " + j);
+//					break outerLoop;
+//				}
+//				
+//			}
+//		}
 		
 		
 	}
